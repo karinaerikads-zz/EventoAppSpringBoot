d968fc70f35e8e67cafd2f6e14ccae203f3bc406
Karina Erika <karinaerikads@hotmail.com>
2019-08-26 06:23:47 -0300

    Criação da entidade e formulário para Convidado

--- eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/models/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.eventoapp.models;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable {
@@ -20,6 +22,10 @@ public class Evento implements Serializable {
 	private String local;
 	private String data;
 	private String horario;
+	
+	//Um evento para muitos convidados
+	@OneToMany
+	private List<Convidado> convidados;
 	
 	public long getCodigo() {
 		return codigo;
