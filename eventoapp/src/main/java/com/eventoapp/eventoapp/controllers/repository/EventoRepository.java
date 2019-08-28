7f3b4a56ee5099c8a9a2d9622e97e6c1607e54b6
Karina Erika <karinaerikads@hotmail.com>
2019-08-28 19:22:02 -0300

    Projeto configurado para deploy com heroku usando postgreslq

--- eventoapp/src/main/java/com/eventoapp/eventoapp/controllers/repository/EventoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eventoapp.eventoapp.controllers.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.eventoapp.eventoapp.models.Evento;
-
-public interface EventoRepository extends CrudRepository<Evento, String> {
-	Evento findByCodigo(long codigo);
-}
