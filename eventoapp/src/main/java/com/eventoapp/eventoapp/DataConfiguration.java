d9a58af54927d292e543a9f61b5693e721b7f6ae
Karina Erika <karinaerikads@hotmail.com>
2019-08-28 19:39:22 -0300

    Projeto configurado para deploy com heroku usando postgreslq [commit com os arquivos]

---
+++ eventoapp/src/main/java/com/eventoapp/eventoapp/DataConfiguration.java
@@ -0,0 +1,60 @@
+package com.eventoapp.eventoapp;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.dialect.Database;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+//import org.springframework.orm.jpa.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+@Configuration
+public class DataConfiguration {
+	
+	/*Para usar o mysql, deve-se utilizar esses dois métodos que estão comentados
+	@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+        return dataSource;
+    }*/
+	
+	//@Bean
+	//public JpaVendorAdapter jpaVendorAdapter(){
+		//HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		//adapter.setDatabase(org.springframework.orm.jpa.vendor.Database.MYSQL);
+		//adapter.setShowSql(true); //Mostra no console a execução de um select, delete etc
+		//adapter.setGenerateDdl(true); //Permite que o hibernate crie as tabelas automaticamente
+		//adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		//adapter.setPrepareConnection(true); //true para preparar a conexão automaticamente
+		//return adapter;
+	//}
+
+	
+	@Bean
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+	
+
+}
+}
